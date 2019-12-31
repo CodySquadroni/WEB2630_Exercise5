e85bd3a2fba68a377e80c395a3355e1ce6477ae9
codysquadroni <codysquadroni@weber.edu>
2019-12-31 12:03:16 -0700

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# exercise5
+# WEB 2630 Exercise 5
+> Forms are another integral part of many web applications. In this Assignment, you'll practice the usage of them.
 
+## Exercise Steps
+
+1. Fork it (<https://github.com/CodySquadroni/WEB2630_Exercise5/fork>)
+2. Clone it (<https://github.com/GITHUB_USERNAME/WEB2630_Exercise5.git>)
+3. Complete The Exercise Requirements Above
+4. Connect WEB2630_Exercise5 to Netlify
+5. Submit Your GitHub and Netlify URL on Canvas
+
+**Note: Remember to make your GitHub repository private and add me as a collaborator GitHub username: CodySquadroni**
+
+## Final Outcome
+Your final outcome should resemble the class example with your own custom component. There is no image for this due to everyone's looking somewhat different.
+
+# Commands
 ## Project setup
 ```
 npm install
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <form>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <!-- ToDo 1 5 Points-->
+          <!-- Create a Signup Form where you retrieve the following Information -->
+          <!-- Full Name (First Name + Last Name) -->
+          <!-- Mail -->
+          <!-- Password -->
+          <!-- Store Data? Yes/No -->
+
+          <!-- ToDo 2 5 Points -->
+          <!-- Only display the Form if it has NOT been submitted -->
+          <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+          <!-- ToDo 3 5 Points -->
+          <!-- Create a custom form field component -->
+          <!-- This should be something like we did in class but not the same -->
+        </div>
+      </div>
+    </form>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Full Name: </p>
+            <p>Mail: </p>
+            <p>Password: </p>
+            <p>Store in Database?: </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script></script>
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
